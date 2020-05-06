005f532ef16077dd606aaf126380a25cce86c223
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2020-05-06 22:35:44 +0900

    test
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

---
+++ with-sscanf.c
@@ -0,0 +1,45 @@
+#include "include/picotls/fusion.h"
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+/*
+    static const uint8_t key[16] __attribute__ ((aligned(64))) = {};
+    //static const uint8_t key[16] = {};
+    static const uint8_t iv[12] __attribute__ ((aligned(64))) = {};
+    //static const uint8_t iv[12]  = {};
+    static const uint8_t aad[13] __attribute__ ((aligned(64))) = {};
+    //static const uint8_t aad[13] = {};
+    static const uint8_t text[16384] __attribute__ ((aligned(64))) = {};
+    //static const uint8_t text[16384] = {};
+    ptls_fusion_aesgcm_context_t ctx __attribute__ ((aligned(64)));
+    //ptls_fusion_aesgcm_context_t ctx;
+    uint8_t encrypted[sizeof(text) + 16] __attribute__ ((aligned(64)));
+    //uint8_t encrypted[sizeof(text) + 16];
+    size_t textlen __attribute__ ((aligned(64))) = 16384;
+    //size_t textlen = 16384;
+*/
+    static const uint8_t key[16] = {}, iv[12] = {}, aad[13] = {}, text[16384] = {};
+    ptls_fusion_aesgcm_context_t ctx;
+    uint8_t encrypted[sizeof(text) + 16];
+    size_t textlen = 16384;
+
+    // 以下のifを追加すると0.7秒遅くなる
+    if (sscanf(argv[1], "%zu", &textlen) != 1) {
+        fprintf(stderr, "failed to obtain text length from argument\n");
+        return 1;
+    }
+
+    ptls_fusion_aesgcm_init(&ctx, key);
+
+    for (int i = 0; i < 1000000; ++i)
+        ptls_fusion_aesgcm_encrypt(&ctx, iv, aad, sizeof(aad), encrypted, text, textlen);
+
+
+    // このprintfにはバグがあり、sizeof(text)をtextlenに変えると、上掲の速度低下はなくなる
+    for (int i = 0; i < 16; ++i)
+        printf("%02x", encrypted[sizeof(text) + i]);
+    printf("\n");
+
+    return 0;
+}
